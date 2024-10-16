0d6b0a96a239ede0c89ee4189e9e92a2942cb54f
DeAffa <muhammaddaffanurahman44@student.cs.unida.gontor.ac.id>
2024-10-16 09:15:57 +0700

    Update Dashboard.py

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 # Load dataset
 # Menyiapkan data day_df
-hari_df = pd.read_csv("Dashboard\hari_clean.csv")
+hari_df = pd.read_csv("main_data.csv")
 hari_df.head()
 
 st.set_page_config(page_title="Bike-sharing Dashboard",
